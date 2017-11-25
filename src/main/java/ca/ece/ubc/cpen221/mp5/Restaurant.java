1491a0f70990840535bc88ff2fb1565d83c2a3c9
gracez72 <gracezhang72@gmail.com>
2017-11-25 02:01:16 -0800

    restaurant

--- src/main/java/ca/ece/ubc/cpen221/mp5/Restaurant.java
+++ src/main/java/ca/ece/ubc/cpen221/mp5/Restaurant.java
@@ -30,9 +30,6 @@ public class Restaurant extends Business{
 	 * 
 	 */
 	
-	 //Business (String url, String name, String business_id, double longitude, double latitude, int price,
-	 //        String photo_url, int review_count, ArrayList<String> schools, String state, String full_address,
-	 //        boolean open, ArrayList<String> neighbourhoods, String city, String type, ArrayList<String> categories)
 	 
 	public Restaurant (String url, String name, String business_id, double longitude, double latitude, int price,
 	         String photo_url, int review_count, ArrayList<String> schools, String state, String full_address,

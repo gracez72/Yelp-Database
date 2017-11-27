8d94bd1dc6d04aedbfe5d204216679b2ea2cc9e6
gracez72 <gracezhang72@gmail.com>
2017-11-26 16:13:12 -0800

    DBServer started

---
+++ src/main/java/ca/ece/ubc/cpen221/mp5/DBServer.java
@@ -0,0 +1,135 @@
+package ca.ece.ubc.cpen221.mp5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * DBServer is a server that handles request from a client using the YelpDB.
+ * It accepts requests of the form: Request ::= Number "\n" Number ::= [0-9]+
+ * and for each request, returns a reply of the form: Reply ::= (Number | "err")
+ * "\n" where a Number is the request Fibonacci number, or "err" is used to
+ * indicate a misformatted request. DBServer can handle multiple
+ * concurrent clients.
+ * 
+ * FORMAT FROM: FIBONACCISERVER EXAMPLE
+ */
+public class DBServer {
+	/** Default port number where the server listens for connections. */
+	public static int DB_PORT;
+
+	private ServerSocket serverSocket;
+
+	// Rep invariant: serverSocket != null
+
+
+	/**
+	 * Make a DBServer that listens for connections on port.
+	 * 
+	 * @param port
+	 *            port number, requires 0 <= port <= 65535
+	 */
+	public DBServer(int port) throws IOException {
+		serverSocket = new ServerSocket(port);
+		this.DB_PORT = port;
+	}
+
+	/**
+	 * Run the server, listening for connections and handling them.
+	 * 
+	 * @throws IOException
+	 *             if the main server socket is broken
+	 */
+	public void serve() throws IOException {
+		while (true) {
+			// block until a client connects
+			final Socket socket = serverSocket.accept();
+			// create a new thread to handle that client
+			Thread handler = new Thread(new Runnable() {
+				public void run() {
+					try {
+						try {
+							handle(socket);
+						} finally {
+							socket.close();
+						}
+					} catch (IOException ioe) {
+						// this exception wouldn't terminate serve(),
+						// since we're now on a different thread, but
+						// we still need to handle it
+						ioe.printStackTrace();
+					}
+				}
+			});
+			// start the thread
+			handler.start();
+		}
+	}
+
+	/**
+	 * Handle one client connection. Returns when client disconnects.
+	 * 
+	 * @param socket
+	 *            socket where client is connected
+	 * @throws IOException
+	 *             if connection encounters an error
+	 */
+	private void handle(Socket socket) throws IOException {
+		System.err.println("client connected");
+
+		// get the socket's input stream, and wrap converters around it
+		// that convert it from a byte stream to a character stream,
+		// and that buffer it so that we can read a line at a time
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				socket.getInputStream()));
+
+		// similarly, wrap character=>bytestream converter around the
+		// socket output stream, and wrap a PrintWriter around that so
+		// that we have more convenient ways to write Java primitive
+		// types to it.
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(
+				socket.getOutputStream()), true);
+
+		try {
+			// each request is a single line containing a number
+			for (String line = in.readLine(); line != null; line = in
+					.readLine()) {
+				System.err.println("request: " + line);
+				try {
+					YelpDB<Restaurant> db = new YelpDB<Restaurant>("https://raw.githubusercontent.com/CPEN-221/f17-mp51-gracez72_andradazoltan/master/data/restaurants.json?token=Ad5rmo9tXwh9lYalidf_muOfIGcyx4H1ks5aIm-HwA%3D%3D",
+				             "https://raw.githubusercontent.com/CPEN-221/f17-mp51-gracez72_andradazoltan/master/data/users.json?token=Ad5rmtqKeZTfXUn11R35DZcTczpgqLc4ks5aIm_WwA%3D%3D",
+				             "https://raw.githubusercontent.com/CPEN-221/f17-mp51-gracez72_andradazoltan/master/data/reviews.json?token=Ad5rmsox3KwRPuEEBRwJq6p-rHsUg5mmks5aIm_DwA%3D%3D");
+					db.getMatches(line);
+					System.err.println("reply: " + line);
+					out.println(line);
+				} catch (NumberFormatException e) {
+					// complain about ill-formatted request
+					System.err.println("reply: err");
+					out.print("err\n");
+				}
+				// important! our PrintWriter is auto-flushing, but if it were
+				// not:
+				// out.flush();
+			}
+		} finally {
+			out.close();
+			in.close();
+		}
+	}
+
+	/**
+	 * Start a DBServer running on the default port.
+	 */
+	public static void main(String[] args) {
+		try {
+			DBServer server = new DBServer(4949);
+			server.serve();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}

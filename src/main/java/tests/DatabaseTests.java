42b23c92224ae005922d53659d5fd93631003c42
unknown <andrada.9@hotmail.com>
2017-11-27 19:00:32 -0800

    Finished kCLusters

--- src/main/java/tests/DatabaseTests.java
+++ src/main/java/tests/DatabaseTests.java
@@ -18,10 +18,10 @@ public class DatabaseTests {
 	}
 	
 	@Test
-	public void testKCluster() {
-		MP5Db db = new YelpDB("data/restaurantsSmaller",
+	public void testKCluster() throws InvalidBusinessException {
+		YelpDB<Restaurant> db = new YelpDB<Restaurant>("https://raw.githubusercontent.com/CPEN-221/f17-mp51-gracez72_andradazoltan/master/data/restaurants.json?token=Ad5rmo9tXwh9lYalidf_muOfIGcyx4H1ks5aIm-HwA%3D%3D",
 				"https://raw.githubusercontent.com/CPEN-221/f17-mp51-gracez72_andradazoltan/master/data/users.json?token=Ad5rmtqKeZTfXUn11R35DZcTczpgqLc4ks5aIm_WwA%3D%3D",
 	             "https://raw.githubusercontent.com/CPEN-221/f17-mp51-gracez72_andradazoltan/master/data/reviews.json?token=Ad5rmsox3KwRPuEEBRwJq6p-rHsUg5mmks5aIm_DwA%3D%3D");
-		db.kMeansClusters_json(2);
+		db.kMeansClusters_json(20);
 	}
 }
